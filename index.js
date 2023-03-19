bd4361f3e4304efa20e40c8620342d9a746085cb
Joab Owala <owalajoab@gmail.com>
2023-03-20 01:09:23 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+   let finalResult = record.find(record=>record.result==="W");
+   if(finalResult){
+    return finalResult.year;
+   }else{
+    return undefined;
+   }
+}
